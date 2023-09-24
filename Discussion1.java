6cd8849da4246eccd899eb0d5e3a494305e68d03
brandongerber <90160923+brandongerber@users.noreply.github.com>
2023-09-24 15:04:37 -0400

    Add files via upload

---
+++ Discussion1.java
@@ -0,0 +1,16 @@
+public class Discussion1 {
+	
+	public static void main(String[] args) {
+		   String name = "John Doe";
+	        String address = "1 Ocean Drive Miami Beach 33139"; // I believe this is the Zip Code for Miami, i was just guessing.
+	        String phoneNumber = "9543053055";
+	        String collegeMajor = "Cybersecurity";
+
+	        String allInformation = name + "\n" + address + "\n" + phoneNumber + "\n" + collegeMajor;
+
+	        System.out.println(allInformation);
+	    }
+
+	    }
+
+				
